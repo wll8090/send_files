a700ee016e19cc3ebb4559e5efdf032672ee86d4
sergio williams <wllyvn@gmail.com>
2023-09-24 16:06:18 -0300

    add files image

--- app.py
+++ app.py
@@ -5,7 +5,20 @@ def creat_app():
     app=Flask(__name__)
     path='./static'
 
+    @app.route('/')
+    def index():
+        text='<h2>file_list</h2><br>'
+        for i in listdir(path):
+            text += f"{i}<br>"
+        return text
+    @app.route("/<file>")
+    def sendFile(file):
+        print(file)
+        file=f'{path}/{file}'
+        return send_file(file)
 
     return app
 
 app=creat_app()
+
+app.run(debug=1)
